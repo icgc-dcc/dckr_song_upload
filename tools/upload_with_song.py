00a6c7ec625c31b5a3146a883668024daf60cd91
Hardeep <hardeep.nahal@oicr.on.ca>
2020-08-28 12:51:12 -0400

    capture error

--- tools/upload_with_song.py
+++ tools/upload_with_song.py
@@ -12,6 +12,8 @@ import subprocess
 import requests
 
 
+log = open("log.txt", "w")
+
 def create_manifest(api, analysis_id, payload_file, manifest_file,files_dir):
     payload = json.load(open(payload_file))
 
@@ -88,7 +90,7 @@ def main():
     create_manifest(api,analysis_id,payload_file,manifest_filename,results.input_dir)
 
     if not api.get_analysis(analysis_id).__dict__['analysisState'] == "PUBLISHED":
-        subprocess.check_output(['score-client','upload','--manifest',os.path.join(results.input_dir,manifest_filename), '--force'])
+        subprocess.check_output(['score-client','upload','--manifest',os.path.join(results.input_dir,manifest_filename), '--force'], stderr=log)
 
     response = requests.put(server_url+'/studies/'+study_id+'/analysis/publish/'+analysis_id, headers={'Authorization':'Bearer '+access_token})
     if response.status_code > 300:
